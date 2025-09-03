5d8dfcc0731e18ebdab4ff1bc7fd138873a42552
brunogoulart2025 <brunogoulartdoss@gmail.com>
2025-09-03 17:48:49 -0300

    Update script.js

--- EventHub/js/script.js
+++ EventHub/js/script.js
@@ -1,1 +1,11 @@
-teste
+document.addEventListener("DOMContentLoaded", () => {
+  const botao = document.getElementById("botao");
+  const texto = document.getElementById("texto");
+
+  botao.addEventListener("click", () => {
+    texto.textContent =
+      texto.textContent === "Confira os principais eventos do mês!"
+        ? "Novo evento em breve, fique ligado!"
+        : "Confira os principais eventos do mês!";
+  });
+});
